5e4864ce7f7c3aa3f0a8edf18ad49c0c2d5f9ece
David Pierce <david.dean.pierce@gmail.com>
2019-07-02 09:35:46 -0700

    [Ep 1] Solve our memory problem by emitting values with generators

--- ep-1-data-as-streams/fib.js
+++ ep-1-data-as-streams/fib.js
@@ -26,12 +26,46 @@ function fib (elements = 0) {
  * larger on your computer, but because the 'fib' function builds up its result
  * in one place, it needs to find enough memory to hold everything before it can
  * return. Eventually, you'll run out of memory and this will break.
+ *
+ * ========= COMMENTED OUT SO WE CAN WORK WITH GENERATORS INSTEAD ==========
  */
-for (const num of fib(10000000000)) {
-  console.log(num);
-}
+//for (const num of fib(10000000000)) {
+//  console.log(num);
+//}
 
 // Data as streams
+
+/**
+ * fibGen takes advantage of the "generator" construct to let us define a
+ * sequence and emit each value one at a time as the generator is consumed.
+ *
+ * With this, we don't accumulate our result in one place. Instead, we only need
+ * as much memory as is required to walk through the problem and emit each
+ * result as it is calculated. This also means we can consume it iteratively on
+ * the other side rather than wait for the entire data to be calcuated before we
+ * can work with it.
+ */
+function* fibGen (elements = 0) {
+  let a = 0;
+  let b = 1;
+
+  yield 1;
+  while (elements > 0) {
+    const c = a + b;
+    a = b;
+    b = c;
+    yield c.toString();
+    elements--;
+  }
+}
+
+// This walks the generator and prints each number one at a time. Node.js will
+// run out of its ability to represent large numbers before it runs out of
+// memory in this problem -- that is a problem, but not the focus of this video
+// :)
+for (const num of fibGen(10000000000)) {
+  console.log(num);
+}
 
 // Consuming and sharing streams
 
